226181de1c16d0154f72c418c4087a0e6163c4e4
Mariusz Heyda <insanepl@gmail.com>
2018-07-18 14:02:32 +0200

    Add Variables + Some of the helpers

--- src/components/app.js
+++ src/components/app.js
@@ -1,24 +1,28 @@
 import { h, Component } from 'preact';
 import { Router } from 'preact-router';
+import WebFont from 'webfontloader';
 
-import Header from './header';
-
-// Code-splitting is automated for routes
 import Home from '../routes/home';
+
+WebFont.load({
+  custom: {
+    families: ['Gilroy:n1,n3,n5'],
+    urls: ['assets/fonts/fonts.css'],
+  },
+});
 
 export default class App extends Component {
   /** Gets fired when the route changes.
    *  @param {Object} event    "change" event from [preact-router](http://git.io/preact-router)
    *  @param {string} event.url  The newly routed URL
    */
-  handleRoute = (e) => {
-    this.currentUrl = e.url;
+  handleRoute = (event) => {
+    this.currentUrl = event.url;
   };
 
   render() {
     return (
       <div id='app'>
-        <Header />
         <Router onChange={this.handleRoute}>
           <Home path='/' />
         </Router>
