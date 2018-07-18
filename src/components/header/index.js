92b9cd34eff1ced052649986aed14a8aa2b1a2e7
Maciej Sykuła <maciek.sykula@gmail.com>
2018-07-18 11:31:19 +0200

    Replace boilerplater with new app created with preact-cli

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,18 +1,16 @@
-import { h, Component } from 'preact';
-import { Link } from 'preact-router';
-import style from './style.less';
+import { h } from 'preact';
+import { Link } from 'preact-router/match';
+import style from './style.css';
 
-export default class Header extends Component {
-	render() {
-		return (
-			<header class={style.header}>
-				<h1>Preact App</h1>
-				<nav>
-					<Link href="/">Home</Link>
-					<Link href="/profile">Me</Link>
-					<Link href="/profile/john">John</Link>
-				</nav>
-			</header>
-		);
-	}
-}
+const Header = () => (
+  <header className={style.header}>
+    <h1>Preact App</h1>
+    <nav>
+      <Link activeClassName={style.active} href='/'>Home</Link>
+      <Link activeClassName={style.active} href='/profile'>Me</Link>
+      <Link activeClassName={style.active} href='/profile/john'>John</Link>
+    </nav>
+  </header>
+);
+
+export default Header;
