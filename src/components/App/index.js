c6fa3e8d9835146319f6b150129bbeaa993dd3d0
Mariusz Heyda <insanepl@gmail.com>
2018-07-23 13:48:58 +0200

    Webfont loader fix for SSR application

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,5 @@
 import { Component } from 'preact';
 import { Router } from 'preact-router';
-import WebFont from 'webfontloader';
 import Home from '../../routes/home';
 import Header from '../Header';
 import Wrapper from '../Wrapper';
@@ -8,7 +7,10 @@ import Interested from '../../routes/interested/index';
 import ProtectedRoute from '../ProtectedRoute';
 import routeMap from '../../utils/routeMap';
 
-if (typeof window !== 'undefined') {
+const isBrowser = typeof window !== 'undefined';
+const WebFont = isBrowser ? require('webfontloader') : undefined;
+
+if (isBrowser) {
   WebFont.load({
     custom: {
       families: ['Gilroy:n1,n3,n5'],
