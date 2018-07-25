c1491ed8b555e64b3f2f8c188fec213efb7ccc6f
Mariusz Heyda <insanepl@gmail.com>
2018-07-25 15:52:22 +0200

    Your Place View (WIP)

--- src/routes/yourplace/index.js
+++ src/routes/yourplace/index.js
@@ -1,18 +1,57 @@
 import { Component } from 'preact';
+import Heading from '../../components/Heading';
+import Button from '../../components/Button/index';
+import PlaceNumber from '../../components/PlaceNumber';
 
 export default class YourPlace extends Component {
-  state = { placeNumber: 666 };
+  state = {
+    placeNumber: 1337,
+    placeIsAvailable: false,
+  };
 
   componentDidMount () {
     // Get Current User Place and push to state
   }
 
+  releasePlace = () => {
+    this.setState({
+      placeIsAvailable: true,
+    });
+  };
+
+  renderPlaceAvailable = placeNumber => (
+    <div className='sth'>
+      <Heading> Twoje miejsce: </Heading>
+      <PlaceNumber number={placeNumber} />
+      <Button
+        Primary
+        href='/'
+        onClick={this.releasePlace}
+      >
+        Zwolnij miejsce
+      </Button>
+    </div>
+  );
+
+  renderPlaceReleased = placeNumber => (
+    <div className='sth'>
+      <Heading> Zwolniłeś miejsce na jeden dzień </Heading>
+      <PlaceNumber number={placeNumber} />
+      <Button
+        Primary
+        href='/'
+      >
+        Odwołaj zwolnienie miejsca
+      </Button>
+    </div>
+  );
+
   render () {
     const { state } = this;
     return (
-      <div>
-        {state.placeNumber}
-      </div>
+      !state.placeIsAvailable
+        ? this.renderPlaceAvailable(state.placeNumber)
+        : this.renderPlaceReleased(state.placeNumber)
     );
   }
 }
