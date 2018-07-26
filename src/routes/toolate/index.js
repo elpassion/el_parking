1d3c491fa2c5010021dfe706d3f1f98a3bbf3f32
Mariusz Heyda <insanepl@gmail.com>
2018-07-26 14:12:26 +0200

    Front Car Variant (could be used with fills instead of whole svg but designer need to simplify it)

--- src/routes/toolate/index.js
+++ src/routes/toolate/index.js
@@ -1,19 +1,19 @@
 import Heading from '../../components/Heading';
 import Hint from '../../components/Hint';
+import FrontCar from '../../components/Svg/FrontCar';
 
 import style from './style.scss';
 
 const TooLate = () => (
   <div>
-    <div>
-      <Heading color='Blue'>
-        Niestety ktoś Cię wyprzedził
-      </Heading>
-      <Hint
-        text={['i miejsce udostępnione wcześniej, zostało już zajętę. Nie trać nadziei.']}
-        color='gray'
-      />
-    </div>
+    <FrontCar LightBackground />
+    <Heading color='Blue'>
+      Niestety ktoś Cię wyprzedził
+    </Heading>
+    <Hint
+      text={['i miejsce udostępnione wcześniej, zostało już zajętę. Nie trać nadziei.']}
+      color='gray'
+    />
   </div>
 );
 
